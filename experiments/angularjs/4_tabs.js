b88d669d0bced80f1879fa3bb0752a529b36ad0e
Huihong Wang <hhwangcocora@gmail.com>
2015-02-15 17:45:07 -0500

    Finish week 4 experiment 4

---
+++ experiments/angularjs/4_tabs.js
@@ -0,0 +1,18 @@
+/**
+ * Created by hhwang on 2/15/15.
+ */
+var app = angular.module('tabsApp', [])
+app.controller('tabsController', function ($scope){
+
+    this.activeTab = 1
+
+    this.show = function (tabName) {
+        return tabName == this.activeTab
+    }
+    this.click = function (tabName) {
+        this.activeTab = tabName
+    }
+    
+
+})
+
