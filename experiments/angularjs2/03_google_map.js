d033b2a6823a295185c6f9a8b342d11edf321b17
Huihong Wang <hhwangcocora@gmail.com>
2015-02-20 23:26:53 -0500

    week 5 experiment 3 done

--- experiments/angularjs2/03_google_map.js
+++ experiments/angularjs2/03_google_map.js
@@ -3,22 +3,35 @@
  */
 var app = angular.module('MapApp', ['uiGmapgoogle-maps'])
 app.controller('MapController', function ($scope){
+    if (google.loader.ClientLocation) {
+        $scope.latitude = google.loader.ClientLocation.latitude
+        $scope.longitude = google.loader.ClientLocation.longitude
+    } else {
+        $scope.latitude = 42
+        $scope.longitude = -72
+    }
+
     $scope.map = {
-        center: {latitude: 45, longitude: -73},
-        zoom: 8
+        center: {latitude: $scope.latitude, longitude: $scope.longitude},
+        zoom: 12
     }
 
     $scope.marker = {
         id: 0,
         coords: {
-            latitude: 45,
-            longitude: -73
+            latitude: $scope.latitude,
+            longitude: $scope.longitude
         },
         options: {
             draggable: true
         },
         events: {
-            
+            dragend: function(event) {
+                $scope.latitude = event.position.k
+                $scope.longitude = event.position.D
+                $scope.map['center'] = {latitude: $scope.latitude, longitude: $scope.longitude}
+
+            }
         }
     }
     /*$scope.searchbox = {
