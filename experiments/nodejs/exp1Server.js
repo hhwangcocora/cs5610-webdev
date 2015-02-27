9756056c8e4976e5e8eeb63f7d372eeceed231a3
Huihong Wang <hhwangcocora@gmail.com>
2015-02-26 20:13:56 -0500

    week 6 experiment 1 done

--- experiments/nodejs/exp1Server.js
+++ experiments/nodejs/exp1Server.js
@@ -8,30 +8,66 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-var developers = [
-{firstName: 'Alice', lastName: 'Wonderland', application: {name: ['aa', 'bb']}},
-{firstName: 'Bob', lastName: 'J', application: {name: ['cc', 'dd']}}
+var currentUser = 'bob'
+
+// data
+var registeredCourses = {
+    bob: [1, 3]
+}
+
+var courses = [
+    {'number':'CS5600', 'name':'Computer System', 'campus':'Boston', 'professor':'Nathaniel Tuck',
+        'time':'W: 10:00am to 12:00pm','classroom':'WVH 220'},
+    {'number':'CS5610', 'name':'Web Development', 'campus':'Boston', 'professor':'Jose G. Annunziato',
+        'time':'WF: 11:45am to 1:25pm','classroom':'WVH 110'},
+    {'number':'CS5800', 'name':'Algorithm', 'campus':'Seattle', 'professor':'Ravi Sundaram',
+        'time':'T: 6:00pm to 9:00pm','classroom':'WVH 120'},
+    {'number':'CS5700', 'name':'Fundamentals of Computer Networking', 'campus':'Boston', 'professor':'David Ross Choffnes',
+        'time':'MT: 1:45pm to 3:25pm','classroom':'SH 108'},
+    {'number':'CS6240', 'name':'Parallel Data Processing in MapReduce', 'campus':'Boston', 'professor':'Jan Vitek',
+        'time':'WF: 11:45am to 1:25pm','classroom':'EVH 440'},
+    {'number':'CS6510', 'name':'Advanced Software Development', 'campus':'Online', 'professor':'James Slocum Miller',
+        'time':'WF: 11:45am to 1:25pm','classroom':'MH 110'}
 ]
 
-// web service endpoints
-app.get('/developers/:id/application', function (req, res) {
-    // req.params.index or req.params['index']
-    // /entityType/ID
-    // /entityTyp
-    var id = req.params.id
-    res.json(developers[id].application)
+// helper functions
+var getRegisteredCourse = function() {
+    var courseIds = registeredCourses[currentUser]
+    var result = []
+    for (var idx in courseIds) {
+        result.push(courses[courseIds[idx]])
+    }
+    return result
+}
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/exp1.html')
 })
 
-app.delete('/developer/:id', function(req, res) {
-    var id = req.params.id
-    developers.splice(id, 1)
-    res.json(developers)
+// Get the user's registered courses
+app.get('/registeredCourses', function (req, res) {
+    res.json(getRegisteredCourse())
 })
 
-app.post('/developer', function(req, res) {
-    var obj = req.body
-    developers.push(obj)
-    res.json(developers)
+// Get all courses
+app.get('/allCourses', function (req, res) {
+    res.json(courses)
+})
+
+// Unregister a course
+app.delete('/:courseid', function(req, res) {
+    var courseId = req.params.courseid
+    registeredCourses[currentUser].splice(registeredCourses[currentUser].indexOf(courseId), 1)
+    res.json(getRegisteredCourse())
+})
+
+// Register a new course
+app.post('/:courseid', function(req, res) {
+    var courseId = req.params.courseid
+    if (registeredCourses[currentUser].indexOf(courseId) < 0) {
+        registeredCourses[currentUser].push(courseId)
+    }
+    res.json(getRegisteredCourse())
 })
 
 app.listen(3000)
